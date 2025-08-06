98997ea0d3d2b3e03398d221d7759e43992e83c8
Muhammad Risqi Risfianda <60572826+RisqiRisfianda@users.noreply.github.com>
2025-08-06 12:05:33 +0700

    Update total_transactions_per_user.sql

--- sql/total_transactions_per_user.sql
+++ sql/total_transactions_per_user.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+  client_id,
+  COUNT(*) AS total_transactions,
+  SAFE_CAST(COUNT(*) / COUNT(DISTINCT FORMAT_DATE('%Y-%m', DATE(date))) as INT) AS avg_monthly_transactions,
+  ROUND(SUM(CAST(LTRIM(amount, '$') AS FLOAT64)),2) AS total_amount,
+  ROUND(AVG(CAST(LTRIM(amount, '$') AS FLOAT64)),2) AS avg_transaction_amount
+FROM 
+  transactions_data
+GROUP BY
+  client_id
